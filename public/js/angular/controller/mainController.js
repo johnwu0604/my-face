9d7a1519e26c25c0ee10b717a48ee886bbf4ed73
John Wu <john.wu@mail.mcgill.ca>
2017-12-02 10:42:52 -0500

    finished email

--- public/js/angular/controller/mainController.js
+++ public/js/angular/controller/mainController.js
@@ -5,6 +5,7 @@ angular.module('mainController', [])
         $scope.facebook_user_data = {};
         $scope.loading = true;
         $scope.emailFormData = {};
+        $scope.email_success_message = '';
 
         var token = 'EAACEdEose0cBAAmvea3nyvOkqruy9JKOfZAy0RcX6mG5xNdsaugKZBjYkVlcfYe8ZAYa7QG51N9XJljEZBTQUB9FpkmLoHrnh3h7XtVh6DP5ViaQnAo9OgZCE27ZAj5BfuBgNi8NuaADnbZAULFq2ezTXN7BFnZBT3fUyG68xOLQnkogS9K1MKpfqvmqojQ0PZCoApAwTSv3ufgZDZD';
 
@@ -17,12 +18,11 @@ angular.module('mainController', [])
 
 
         $scope.sendEmail = function() {
-
+            $scope.emailFormData.user_email = $scope.facebook_user_data.user_info.basic_info.email;
             Email.create($scope.emailFormData).success(function(result) {
-
+                $scope.emailFormData = {};
+                $scope.email_success_message = 'Your email was successfully sent!';
             })
-
-
         };
 
 
