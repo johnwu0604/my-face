0d3bcb12e5a3ce78274bde7637fef821dbbe0861
John Wu <john.wu@mail.mcgill.ca>
2017-12-02 15:04:07 -0500

    fsd

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,13 @@
 var passport = require('passport');
-var Strategy = require('passport-facebook').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 var CircularJSON = require('circular-json')
 
-passport.use(new Strategy({
+passport.use(new FacebookStrategy({
         clientID: '1546707545396988',
         clientSecret: '7ff1392c924c3e724f28734e9c907ace',
         callbackURL: '/test'
     },
     function(accessToken, refreshToken, profile, cb) {
-        // In this example, the user's Facebook profile is supplied as the user
-        // record.  In a production-quality application, the Facebook profile should
-        // be associated with a user record in the application's database, which
-        // allows for account linking and authentication with other identity
-        // providers.
         var user = {
             'email': profile.emails[0].value,
             'name' : profile.name.givenName + ' ' + profile.name.familyName,
@@ -35,14 +30,17 @@ module.exports = function(app) {
     app.use(passport.initialize());
     app.use(passport.session());
 
-    app.get('/auth/facebook',
-        passport.authenticate('facebook'));
-
     app.get('/auth/facebook/callback',
         passport.authenticate('facebook', { failureRedirect: '/login' }),
         function(req, res) {
             // Successful authentication, redirect home.
             res.redirect('/');
+        });
+
+    app.get('/auth/facebook',
+        passport.authenticate('facebook'), function(req, res){
+        console.log('Callback Test: ' + req)
+            res.send(req);
         });
 
     app.get('/test', function(req, res) {
