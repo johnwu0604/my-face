4173ac8ab8498417112e4c2a478c5f88ff1388de
John Wu <john.wu@mail.mcgill.ca>
2017-12-02 18:54:39 -0500

    playing around

--- public/js/angular/controller/mainController.js
+++ public/js/angular/controller/mainController.js
@@ -1,3 +1,33 @@
+var init = function () {
+    // do something
+}
+
+var login = function() {
+    var permissions = ['user_photos', 'email', 'user_about_me', 'user_birthday', 'user_education_history', 'user_friends',
+        'user_hometown', 'user_likes', 'user_location', 'user_posts', 'user_relationships', 'user_relationship_details',
+        'user_work_history'];
+    Facebook.login(function(response) {
+        if (response.status == 'connected') {
+            console.log('Result: ' + JSON.stringify(response));
+            $scope.token = response.authResponse.accessToken;
+            $scope.loginStatus = response.status;
+            $scope.getUserData();
+        } else {
+            $scope.status = 'Failed to connect with facebook';
+        }
+    }, { scope: permissions.join(', '), return_scopes: true });
+}
+
+var logout = function() {
+    Facebook.api({
+        method: 'Auth.revokeAuthorization'
+    }, function(response) {
+        Facebook.getLoginStatus(function(response) {
+            $scope.loginStatus = response.status;
+        });
+    });
+}
+
 angular.module('mainController', ['facebook'])
 
     .config(function(FacebookProvider) {
@@ -19,7 +49,7 @@ angular.module('mainController', ['facebook'])
 
         $scope.token = '';
 
-        $scope.init = function() {
+        this.$onInit = function () {
             $scope.removeAuth();
             $scope.login();
         };
@@ -66,8 +96,6 @@ angular.module('mainController', ['facebook'])
                 });
             });
         };
-
-
 
 
     }]);
