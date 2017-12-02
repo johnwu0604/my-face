12c302121e379e951f22e6b73e7a52959af713ce
tiff-wang <wangtiffany@gmail.com>
2017-12-02 08:58:36 -0500

    i don't know what I changed

--- routes/fb.js
+++ routes/fb.js
@@ -2,7 +2,6 @@ var async = require('async')
 
 var FacebookController = require('../api/controller/fbController')
 
-
 module.exports = function(app) {
     app.get('/fb-user-data/:token', function(req, res){
         FacebookController.getUserInformation(req.params.token, function(result) {
@@ -10,3 +9,5 @@ module.exports = function(app) {
         })
     })
 }
+
+
