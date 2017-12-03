335804cbb620ea5d09bd6d2daee19ee5148316e3
John Wu <john.wu@mail.mcgill.ca>
2017-12-02 21:14:25 -0500

    Fixed error

--- public/js/angular/controller/mainController.js
+++ public/js/angular/controller/mainController.js
@@ -23,7 +23,7 @@ angular.module('mainController', ['facebook'])
         }
 
         $scope.exportCtrl = function(){
-            console.log($(result.documentElement).prop(outerHTML))
+            console.log($($scope.result.documentElement).prop(outerHTML))
             var data = {'html': $scope.result.documentElement.outerHTML}
             FacebookService.postWebsiteData(data);
         }
