145666ecca7afa0e68857a096f143c03fdfe6521
John Wu <john.wu@mail.mcgill.ca>
2017-12-02 22:10:59 -0500

    Added amazon stuff

--- api/service/exportService.js
+++ api/service/exportService.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 module.exports = {
     postWebsiteData: function(data, callback){
         console.log(JSON.stringify(data.html));
-        fs.writeFile('/template/index.html', JSON.stringify(data.html), function (err) {
+        fs.writeFile('template/index.html', JSON.stringify(data.html), function (err) {
             if (err) return console.log(err);
             AmazonUtility.uploadDir('../../template', function() {
                 return callback()
