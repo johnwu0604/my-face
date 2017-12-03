f241b882297f4fcfe21d20ef8122355417b28700
John Wu <john.wu@mail.mcgill.ca>
2017-12-03 02:12:24 -0500

    Minor change

--- api/util/amazonUtility.js
+++ api/util/amazonUtility.js
@@ -39,7 +39,7 @@ module.exports = {
         uploader.on('end', function() {
             console.log("done uploading")
             return callback(url)
-        });
+        })
     }
 
 }
--- routes/export.js
+++ routes/export.js
@@ -3,6 +3,7 @@ var ExportController = require('../api/controller/exportController')
 module.exports = function(app) {
     app.post('/website/', function(req, res){
         ExportController.postWebsiteData(req.body, function(url) {
+            console.log('URL: '+ url)
             res.send(url)
         })
     })
